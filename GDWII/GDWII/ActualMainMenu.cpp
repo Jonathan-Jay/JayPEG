39b9f0bc3c523eb7252adc1bb94e552f1c6da138
Leo Buono <leo.buono@ontariotechu.net>
2020-03-27 19:11:32 -0400

    Added Credits text, but no return button
    Added the Credits text, No return to menu button However

--- GDWII/GDWII/ActualMainMenu.cpp
+++ GDWII/GDWII/ActualMainMenu.cpp
@@ -133,6 +133,22 @@ void ActualMainMenu::InitScene(float windowWidth, float windowHeight)
 		unsigned int bitHolder = EntityIdentifier::SpriteBit() | EntityIdentifier::TransformBit();
 		ECS::SetUpIdentifier(entity, bitHolder, "Background 2");
 	}
+	{
+		auto entity = ECS::CreateEntity();
+
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+
+		std::string filename = "Credits.png";
+
+		ECS::GetComponent<Sprite>(entity).LoadSprite(filename, 247*2, 204*2, false);
+
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(1250.f, 25.f, 2.f));
+
+
+		unsigned int bitHolder = EntityIdentifier::SpriteBit() | EntityIdentifier::TransformBit();
+		ECS::SetUpIdentifier(entity, bitHolder, "Credits");
+	}
 
 }
 bool cameraSpan = false;
